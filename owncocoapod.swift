bbfddefff7e7267c23eb4751d0c9504de02e9d17
Duale Abdullahi <abdull53@msu.edu>
2020-04-19 23:11:21 -0500

    Added test cases

---
+++ owncocoapod.swift
@@ -0,0 +1,23 @@
+//
+//  owncocoapod.swift
+//  owncocoapod
+//
+//  Created by Duale on 4/19/20.
+//  Copyright © 2020 Duale. All rights reserved.
+//
+
+import Foundation
+
+public final class owncocoapod {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
