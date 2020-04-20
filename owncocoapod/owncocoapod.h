fc8322ca42594fe2605d96ababf432b872e41fa2
Duale Abdullahi <abdull53@msu.edu>
2020-04-19 22:51:46 -0500

    Initial project setup

---
+++ owncocoapod/owncocoapod.h
@@ -0,0 +1,19 @@
+//
+//  owncocoapod.h
+//  owncocoapod
+//
+//  Created by Duale on 4/19/20.
+//  Copyright © 2020 Duale. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for owncocoapod.
+FOUNDATION_EXPORT double owncocoapodVersionNumber;
+
+//! Project version string for owncocoapod.
+FOUNDATION_EXPORT const unsigned char owncocoapodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <owncocoapod/PublicHeader.h>
+
+
---
+++ owncocoapodexample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  owncocoapodexample
+//
+//  Created by Duale on 4/19/20.
+//  Copyright © 2020 Duale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
